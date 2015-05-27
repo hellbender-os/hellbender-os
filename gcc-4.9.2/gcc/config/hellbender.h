114aa841ed06012431838f595138d7d6fd9d6f75
Hellbender <hellbender.os@gmail.com>
2015-05-27 21:02:42 +0300

    added STARTFILE_SPEC, ENDFILE_SPEC to gcc config

--- gcc-4.9.2/gcc/config/hellbender.h
+++ gcc-4.9.2/gcc/config/hellbender.h
@@ -1,7 +1,16 @@
 /* Useful if you wish to make target-specific gcc changes. */
 #undef TARGET_HELLBENDER
 #define TARGET_HELLBENDER 1
- 
+
+#undef ENDFILE_SPEC
+#define ENDFILE_SPEC "crtend.o%s crtn.o%s"
+
+#undef STARTFILE_SPEC
+#define STARTFILE_SPEC "%{!shared: \
+			 %{!symbolic: \
+			  %{pg:gcrt0.o%s}%{!pg:%{p:mcrt0.o%s}%{!p:crt0.o%s}}}}\
+			crti.o%s crtbegin.o%s"
+
 /* Default arguments you want when running your
    i686-hellbender-gcc/x86_64-hellbender-gcc toolchain */
 #define LIB_SPEC "-lc -lg -lm" /* link against C standard libraries */
