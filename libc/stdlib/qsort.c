962914b7dec69a1ca00445cf65a2c0493a4ee093
Hellbender <hellbender.os@gmail.com>
2015-06-01 01:35:54 +0300

    Fixed crash in 'sort'

--- libc/stdlib/qsort.c
+++ libc/stdlib/qsort.c
@@ -9,14 +9,14 @@ void qsort (void* vbase, size_t num, size_t size,
     char* ptr = base + size;
     for (size_t j = i + 1; j < num; ++j, ptr = ptr + size) {
       int result = (*compare)(min, ptr);
-      if (result < 0) min = ptr;
+      if (result > 0) min = ptr;
     }
     // swap so that the smallest is first.
     if (min != base) {
       for (size_t j = 0; j < size; ++j) {
         char tmp = base[j];
-        base[j] = ptr[j];
-        ptr[j] = tmp;
+        base[j] = min[j];
+        min[j] = tmp;
       }
     }
   }
