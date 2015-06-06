86781460fd3edeb75aa8fbf8dab591ca10b9c0ea
Hellbender <hellbender.os@gmail.com>
2015-06-06 23:56:28 +0300

    made unistd write more posix; added pwrite

--- libc/unistd/write.c
+++ libc/unistd/write.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <limits.h>
 #include <unistd.h>
+#include <errno.h>
 
 #include <coresrv/vfs.h>
 
@@ -9,17 +10,25 @@
 
 ssize_t write(int handle, const void *buffer, size_t size) {
   if (handle < 0 || handle >= OPEN_MAX) {
-    printf("Illegal file handle.\n");
-    abort();
+    errno = EBADF;
+    return -1;
   }
   struct vfs_file *file = &_fcntl_data.handles[handle];
-  if (!file->filesys.open) {
-    printf("File handle not open.\n");
-    abort();
+  if (!file->stat.st_mode) {
+    errno = EBADF;
+    return -1;
   }
   if (!file->filesys.write) {
-    printf("Write not supported.\n");
-    abort();
+    errno = EROFS;
+    return -1;
   }
   return IDC(vfs_write, file->filesys.write, file, buffer, size);
+}
+
+ssize_t pwrite(int handle, const void *buf, size_t nbyte, off_t offset) {
+  off_t current = lseek(handle, 0, SEEK_CUR);
+  lseek(handle, offset, SEEK_SET);
+  ssize_t retval = write(handle, buf, nbyte);
+  lseek(handle, current, SEEK_SET);
+  return retval;
 }
