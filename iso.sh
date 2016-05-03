80468693d7b8b8ed3596ed8850f8123d9017e0e5
Hellbender OS 64 <hellbender.os@gmail.com>
2016-05-03 23:15:38 +0300

    added initrd

--- iso.sh
+++ iso.sh
@@ -4,11 +4,13 @@ set -e
 
 mkdir -p isodir/boot/grub
 cp -r sysroot/boot/* isodir/boot/
+(cd initrd && { find . -type d -printf "%p/\n"; find . \! -type d ; } | sort | cpio -v -o > ../isodir/boot/initrd )
 
 cat > isodir/boot/grub/grub.cfg << EOF
 menuentry "hellbender" {
     multiboot /boot/hellbender
     module /boot/coresrv --coresrv
+    module /boot/initrd --initrd
 }
 EOF
 grub-mkrescue -o hellbender.iso isodir
