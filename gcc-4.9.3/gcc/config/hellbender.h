62ecec526f126baf06d45328d8e9737416d49867
Hellbender OS 64 <hellbender.os@gmail.com>
2016-05-20 00:30:50 +0300

    fixed all makefiles and toolchain to support static, dynamic, and debug versions of libraries

--- gcc-4.9.3/gcc/config/hellbender.h
+++ gcc-4.9.3/gcc/config/hellbender.h
@@ -3,17 +3,17 @@
 #define TARGET_HELLBENDER 1
 
 #undef ENDFILE_SPEC
-#define ENDFILE_SPEC "crtendS.o%s crtn.o%s"
+#define ENDFILE_SPEC "crtendS.o%s crtnS.o%s"
 
 #undef STARTFILE_SPEC
-#define STARTFILE_SPEC "crti.o%s crtbeginS.o%s"
+#define STARTFILE_SPEC "crt0S.o%s crtiS.o%s crtbeginS.o%s"
 
 #undef CC1_SPEC
-#define CC1_SPEC "%{!ffreestanding:-fPIC -fvisibility=hidden}"
+#define CC1_SPEC "%{fPIC|fpic|shared|pie: -fvisibility=hidden}"
 
 /* Default arguments you want when running your
    i686-hellbender-gcc/x86_64-hellbender-gcc toolchain */
-#define LIB_SPEC "%{!g:-lc; :-lg}" /* link against C standard libraries */
+#define LIB_SPEC "--start-group %{fPIC|fpic|shared|pie: %{!g:-lcS -lmS; :-lcSG -lmSG}; :%{!g:-lc -lm; :-lcG -lmG}} --end-group"
 
 /* Don't automatically add extern "C" { } around header files. */
 #undef  NO_IMPLICIT_EXTERN_C
